7c37e53cc4b392d257fdd8ddabc38b2c1e5ba698
shrijithps <p.shrijith@talview.com>
2025-10-18 16:03:10 +0530

    added_image_when_no_poster_is_present

--- omdb-movies/src/components/MovieDetails.tsx
+++ omdb-movies/src/components/MovieDetails.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import no_image from "../assets/no_image.jpg";
 
 export interface Movie{
     Title: string,
@@ -12,13 +13,14 @@ type MovieDetailsProps = {
     movie: Movie;
 }
 const MovieDetails: React.FC<MovieDetailsProps> = ({movie}) => {
-    const {Title, Year, Poster} = movie;
+    const {Title, Year} = movie;
+    const Poster = movie?.Poster === "N/A" ? no_image : movie.Poster;
 
     return (
         <div className="bg-gray-800 text-white rounded-2xl shadow-lg
                  flex flex-col w-72 hover:scale-105 transition-transform cursor-pointer">
 
-            <img src={Poster} className="h-auto w-full"/>
+            <img src={Poster} className="h-96 w-full"/>
 
           <div className="p-4 gap-2">
                 <h2 className="text-xl font-bold">{Title}</h2>
