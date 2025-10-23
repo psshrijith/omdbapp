530559c4cbf9cc037d8a90f686b068ca41456c69
shrijithps <p.shrijith@talview.com>
2025-10-23 19:19:06 +0530

    fixed_build_errors

--- omdb-movies/vite.config.ts
+++ omdb-movies/vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-    tailwindcss()
-      ],
-})
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    resolve: {
+        alias: {
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@utils': path.resolve(__dirname, './src/utils')
+        }
+    }
+});
